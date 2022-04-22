f5fe3a8516e3a43dcdfa9f171edbd14d019661e9
catgoesmeow14 <exoexo1990@gmail.com>
2022-04-22 15:33:20 +0800

    Add validation for Menu, invalid without name, description, and price

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,4 +1,8 @@
 class Menu < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
+    validates :price, presence: true, numericality: { greater_than: 0.00 }
+    validates :description, presence: true, length: { maximum: 150 }
+
     has_many :category_details
     has_many :categories, through: :category_details
 
--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Menu, type: :model do
-  it 'is valid with a name and a price' do
+  it 'is valid with a name, a description, and a price' do
     menu = Menu.new(
       name: 'Mie Ayam Bakso',
       description: 'Indonesian chicken noodles with meatballs.',
@@ -10,7 +10,48 @@ RSpec.describe Menu, type: :model do
 
     expect(menu).to be_valid
   end
+
+  it 'is INVALID without a name' do
+    menu = Menu.new(
+      name: nil,
+      description: 'Indonesian chicken noodles with meatballs.',
+      price: 15000.0
+    )
+
+    menu.valid?
+
+    expect(menu.errors[:name]).to include("can't be blank")
+  end
+
+  it 'is INVALID without a description' do
+    menu = Menu.new(
+      name: 'Mie Ayam Bakso',
+      description: nil,
+      price: 15000.0
+    )
+
+    menu.valid?
+
+    expect(menu.errors[:description]).to include("can't be blank")
+  end
+
+  it 'is INVALID without a price' do
+    menu = Menu.new(
+      name: 'Mie Ayam Bakso',
+      description: 'Indonesian chicken noodles with meatballs.',
+      price: nil
+    )
+
+    menu.valid?
+
+    expect(menu.errors[:price]).to include("can't be blank")
+  end
+
 end
+
+# validates :name, presence: true, uniqueness: true
+# validates :price, presence: true, numericality: { greater_than: 0.00 }
+# validates :description, presence: true, length: { maximum: 150 }
 
 # t.string "name"
 #     t.text "description"
