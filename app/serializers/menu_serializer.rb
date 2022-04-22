bdabd11bf48ee8b350ce806f8a260eb817c68d8d
catgoesmeow14 <exoexo1990@gmail.com>
2022-04-22 20:09:13 +0800

    Changing relationship in menu_serializer

--- app/serializers/menu_serializer.rb
+++ app/serializers/menu_serializer.rb
@@ -1,4 +1,10 @@
 class MenuSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name, :description, :price
+
+  has_many :menus_categories
+  has_many :categories, through: :menus_categories
+
+  has_many :order_details
+  has_many :orders, through: :order_details
 end
